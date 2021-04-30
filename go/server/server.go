dbae6666aabf8495bf2545920f6e23d963ecd9b4
cPidx <capitalpidx@gmail.com>
2021-04-30 21:59:33 +0900

    fix srever

--- go/server/server.go
+++ go/server/server.go
@@ -14,21 +14,26 @@ func Serve(port, libraryPath string) {
 		db, err := pebble.Open(libraryPath+"db", &pebble.Options{})
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
 		}
 		key := []byte("now")
 		if err := db.Set(key, []byte(time.Now().String()), pebble.Sync); err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
 		}
 		value, closer, err := db.Get(key)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
 		}
 		dbres := fmt.Sprintf("%s is %s", key, value)
 		if err := closer.Close(); err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
 		}
 		if err := db.Close(); err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
 		}
 		fmt.Fprintf(w, "Hello from Go\n\nport: %s\nfrom local DB:\n%s\n", port, dbres)
 	})
